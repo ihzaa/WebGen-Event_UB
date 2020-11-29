b6310aab821a85b0d5d3f04905004141ac278c4a
Ihza Ahmad <ihzaahmad0@gmail.com>
2020-11-29 14:53:38 +0700

    fix ads.js error jika null

--- public/user/js/ads.js
+++ public/user/js/ads.js
@@ -14,9 +14,13 @@ function init() {
     fetch(URL.getLatesAd, headerNoCache())
         .then((resp) => resp.json())
         .then((data) => {
-            $("#ads_poster").attr("src", baseImgUrlAds + data.image);
-            $("#ads_desc").html(data.desc);
-            $(".ads").show();
+            if (data == "") {
+                return;
+            } else {
+                $("#ads_poster").attr("src", baseImgUrlAds + data.image);
+                $("#ads_desc").html(data.desc);
+                $(".ads").show();
+            }
         })
         .then(() => setTimeout(init, 90000))
         .catch((err) => console.log(err));
